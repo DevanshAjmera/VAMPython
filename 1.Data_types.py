ec36218187a955d664b76e87298421c54042f3ee
Devansh Ajmera <devanshajmera1012@gmail.com>
2024-10-04 18:53:08 +0530

    File updated

---
+++ 1.Data_types.py
@@ -0,0 +1,23 @@
+# In Python, there are very few data types. 
+
+# 1. int is used to show numeric value.     eg:2500
+# 2. str Used store string value.           eg:"Devansh"
+# 3. float can store decimal value.         eg:percentage = 75.59
+
+# To store the multiple values
+# Array ->  it can store the multiple value of same data types...
+# eg---     friendsName= ["Devansh","Ajmera",123]
+
+# To store the multiple values
+# #List:- It can store multiple value with different data types.
+#         eg- friendsName= ["Devansh","Ajmera",123]
+# #Tuple:- can store multiple values but cant be modified(change).
+# #Set:-
+# #Dictonary(Dict)
+
+# How to find the data type of any variable?
+amount = 2500
+# print()-> to read any statement in python.
+print(amount)
+print(type(amount))
+
