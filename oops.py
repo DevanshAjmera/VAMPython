2c705b257f47732c130d18ac73857d44202353a6
Devansh Ajmera <devanshajmera1012@gmail.com>
2024-10-07 10:57:39 +0530

    Updated

--- oops.py
+++ oops.py
@@ -10,6 +10,7 @@ devansh : Devansh = Devansh()
 print("My age is",devansh.age)
 
 
+
 class Student:
     name = "Devansh"
     email = "d@gmail.com"
@@ -17,7 +18,25 @@ class Student:
         ageInYear = cY-bY
         print(ageInYear)
     def Monthly_Pocket_Money(this,weekly_money):
-        print("Monhly pocket money is",weekly_money*4)
+        totalMoney = weekly_money*4
+        print("My Pocket Money",totalMoney)
+        #print("Monhly pocket money is",weekly_money*4)
 stu : Student = Student()
 stu.findMyAge(2024,2005)
-stu.Monthly_Pocket_Money(200)
+#stu.Monthly_Pocket_Money(200)
+stu.Monthly_Pocket_Money(int(input("Enter Weekly money: ")))
+
+
+
+#Calculate the top speed of the car if each gear increase car speed by 50 km/h.
+class Car:
+    model= 2024
+    gears= 7 
+    first_gear=50
+
+    def top_speed(this, gear):
+        gear = int(input("Enter gear number (1-7) : "))
+        print("The top speed is :",50*gear)
+car: Car = Car()
+car.top_speed()
+#(int(input("Enter gear number (1-7) : ")))
